210dc740d553baee1a5a114f53ce0869a835e565
TomTom <tom@2do-digital.de>
2024-07-16 14:01:51 +0200

    MAJOR: Update Github Actions

--- tracking.js
+++ tracking.js
@@ -188,6 +188,7 @@ class HolisticTracking {
 
 
 window.addEventListener('DOMContentLoaded', (event) => {
+	console.log("UPDATED SUCCESSFULLY")
 	if (typeof dynamicData == "undefined") {
 		const dynamicData = false;
 		const HolisticTrackingInstance = new HolisticTracking(false);
