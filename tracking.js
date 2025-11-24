582e767f9a557033a594fa538090d17b76b78d74
tom2do <tom@2do-digital.de>
2025-11-24 10:10:10 +0100

    MMICPG-6 Broker Links

--- tracking.js
+++ tracking.js
@@ -225,6 +225,7 @@ class HolisticTracking {
 		items.forEach(link => {
 			link.addEventListener('click', (e) => {
 				const brokerName = link.querySelector('.text-size-small').innerText !== 'Externer Link' ? link.querySelector('.text-size-small').innerText : link.href;
+				if(!brokerName) return;
 				console.log(link, link.querySelector('div'), brokerName);
 				this.pushEvent({
 					"event": "dynamic_event",
